88b41df6ea09c59c68b49d8e28774c8ec2006f09
Yliandis <yliandis@mailo.com>
2020-06-09 16:30:25 +0200

    Object.h set up

--- src/Entity/Object/Object.h
+++ src/Entity/Object/Object.h
@@ -2,6 +2,7 @@
 #define GAME_OBJECT_H
 
 #include <string>
+#include <cmath>
 #include <SFML/Graphics/Sprite.hpp>
 
 #include "../Entity.h"
@@ -10,8 +11,24 @@ class Object : public Entity{
     
 public:
     
+    enum ObjectType{
+        
+        Weapon,
+        Helmet,
+        Amulet,
+        ChestPlate,
+        Leggings,
+        Boots,
+        StuffCount,
+        
+        Potion,
+        Loot
+    };
+    
+public:
+    
     Object() = delete;
-    Object(const std::string&, TextureManager*);
+    Object(const std::string&, TextureManager*, std::size_t = 1);
     ~Object();
     
 public:
@@ -20,7 +37,24 @@ public:
     int getBuyPrice() const;
     int getSellPrice() const;
     
+    std::size_t getNumber() const;
+    std::size_t getStackSize() const;
+    bool empty() const;
+    
+    Object& operator+=(std::size_t&);
+    Object& operator+=(Object&);
+    Object& operator-=(std::size_t&);
+    Object& operator-=(Object&);
+    Object& operator++();
+    Object& operator--();
+    
+    virtual int use(std::size_t = 1);
+    virtual ObjectType getObjectType() const;
+    virtual Bonus* getBonus();
+    
 private:
+    
+    virtual void loadFromFile();
     
     void setSprite();
     
@@ -30,7 +64,11 @@ private:
     
     sf::Sprite sprite;
     
+    std::size_t number;
+    
 protected:
+    
+    std::size_t stackSize;
     
     std::string name;
     
